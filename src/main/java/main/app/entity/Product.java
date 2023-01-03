638935cf14a380973ee749ed4f55e001cfc0280e
MeghanaNelakuditi <51887555+MeghanaNelakuditi@users.noreply.github.com>
2023-01-03 10:49:45 -0600

    Initial commit

---
+++ src/main/java/main/app/entity/Product.java
@@ -0,0 +1,55 @@
+package main.app.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "product")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "productId")
+    private long productId;
+
+    @Column(name = "productName")
+    private String productName;
+
+    @Column(name = "price")
+    private Double price;
+
+    public long getProductId() {
+        return productId;
+    }
+
+    public void setProductId(long productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public int getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(int discount) {
+        this.discount = discount;
+    }
+
+    @Column(name = "discount")
+    private int discount;
+
+}
